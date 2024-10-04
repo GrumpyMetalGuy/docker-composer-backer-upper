37911f78d75f88d9c6c29e8a0cab14d8412a5bbd
Grumpy Metal Guy <18551877+GrumpyMetalGuy@users.noreply.github.com>
2024-10-04 12:10:07 +0100

    First fully working version committed.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # docker-composer-backer-upper
-Utility to backup running docker containers that are managed by docker-compose
+
+A utility to back up running docker containers that are managed by docker-compose.
+
+## Setup
+
+To generate the required Pydantic model that the backer-upper uses, run the following command:
+
+```commandline
+uv run datamodel-codegen
+```
+
+This will create a Python object that will be used to read in compose files for processing.
+
+## Usage
+
+Run using the following command:
+
+```commandline
+uv run main <source docker-compse config folder> <folder to write backups to>
+```
---
+++ main.py
@@ -0,0 +1,160 @@
+import argparse
+import json
+import logging
+import os
+import shutil
+from collections.abc import Iterable
+
+import more_itertools
+import yaml
+from docker_composer_v2 import DockerCompose
+
+from compose_model import ComposeSpecification
+
+logger = logging.getLogger(__name__)
+
+
+"""
+A set of common path exclusions for docker volume mappings that should never be backed up.
+"""
+COMMON_EXCLUSIONS = {
+	'/etc/passwd',
+	'/etc/groups',
+	'/etc/localtime',
+	'/var/run/docker.sock',
+	'/etc/os-release',
+}
+
+
+def _get_compose_files() -> Iterable[str]:
+	"""
+	Determine list of compose files to process.
+
+	:return: List of filenames found in the directory ending in .yml.
+	"""
+
+	docker_compose_instance = DockerCompose()
+
+	ls_results_raw = docker_compose_instance.ls(all=True, format='json').call(capture_output=True)
+
+	ls_results = json.loads(ls_results_raw.stdout)
+
+	return more_itertools.collapse(
+		[
+			result['ConfigFiles'].split(',')
+			for result in ls_results
+			if result['Status'].startswith('running')
+		]
+	)
+
+
+def _get_compose_model(compose_filename: str) -> ComposeSpecification:
+	"""
+	For a given compose filename, attempt to build a docker-compose Model object from it.
+
+	:param compose_filename: docker-compose file to generate model for.
+	:return: Pydantic object representing a docker-compose file.
+	"""
+	with open(compose_filename) as handle:
+		compose_file_contents = handle.read()
+
+	# Simple conversion from YAML -> JSON, other libs exist for this purpose, but we'll keep it simple for now
+	yaml_obj = yaml.safe_load(compose_file_contents)
+	json_str = json.dumps(yaml_obj)
+
+	return ComposeSpecification.model_validate_json(json_str)
+
+
+def _backup_volume(args: argparse.Namespace, service_name: str, volume: str):
+	target_backup_folder = os.path.join(args.destination, service_name)
+
+	for backup_counter in range(args.num_backups, -1, -1):
+		if backup_counter:
+			potential_backup_folder = f'{target_backup_folder}.{backup_counter}'
+		else:
+			potential_backup_folder = target_backup_folder
+
+		if os.path.exists(potential_backup_folder):
+			# If it's the last one, nuke it
+			if backup_counter == args.num_backups:
+				shutil.rmtree(potential_backup_folder)
+			else:
+				shutil.move(potential_backup_folder, f'{target_backup_folder}.{backup_counter + 1}')
+
+	shutil.copytree(volume, os.path.join(target_backup_folder, volume[1:]))
+
+
+def _process_compose_file(args: argparse.Namespace, compose_filename: str):
+	compose_model = _get_compose_model(compose_filename)
+
+	for service_name, service in compose_model.services.items():
+		compose_file_volumes = (
+			set([volume.split(':')[0] for volume in service.volumes if volume.startswith('/')])
+			- COMMON_EXCLUSIONS
+		)
+
+		if compose_file_volumes:
+			docker_compose_instance = DockerCompose(file=compose_filename)
+
+			docker_stopped = False
+
+			try:
+				down_results_raw = docker_compose_instance.down().call(capture_output=True)
+
+				docker_stopped = down_results_raw.returncode == 0
+
+				for volume in compose_file_volumes:
+					_backup_volume(args, service_name, volume)
+			except:
+				logger.exception(f'Attempting to bring down {compose_filename}')
+			finally:
+				if docker_stopped:
+					up_results_raw = docker_compose_instance.up(detach=True).call(
+						capture_output=True
+					)
+
+					if up_results_raw.returncode != 0:
+						logger.warning(
+							f'Error restarting docker-compose file {compose_filename}, return code was {up_results_raw.returncode}'
+						)
+
+
+def process(args: argparse.Namespace):
+	"""
+	Main logic execution. Will load in all compose files from the given directory, stop the docker container if
+	running, then back up any files that it can work out the location of, then restart the container again.
+
+	:param args: Program arguments.
+	"""
+
+	compose_files = _get_compose_files()
+
+	for compose_file in compose_files:
+		_process_compose_file(args, compose_file)
+
+
+def main():
+	argument_parser = argparse.ArgumentParser(
+		description='Utility program to manage backups for docker-compose Docker containers'
+	)
+	argument_parser.add_argument(
+		'destination', type=str, help='directory to place backups of container files in'
+	)
+
+	argument_parser.add_argument(
+		'-n',
+		'--num_backups',
+		type=int,
+		help='number of backups to keep for each compose file',
+		default=5,
+		nargs='?',
+		metavar='number',
+	)
+
+	args = argument_parser.parse_args()
+
+	process(args)
+
+
+if __name__ == '__main__':
+	main()
